934868881e0433c613014d200bb26566fa4551c2
Rayhan Muhammad Adha <67584512+Mr22XX@users.noreply.github.com>
2023-11-05 00:25:15 +0700

    Update task4.java

--- task4.java
+++ task4.java
@@ -2,7 +2,7 @@ public class task4 {
     public static void main(String[] args) {
         String [] mahasiswa = {"Rayhan Muhammad Adha", "Ghazi Al-ghifari", "Faros Jundi", "Diffo Ramadhan", "Ammar Sirraj"};
         for (int i=0 ; i<mahasiswa.length; i++){
-            System.out.println("nama mahasiswa : " + mahasiswa[i]);
+            System.out.println(mahasiswa[i]);
         }
     }
 }
